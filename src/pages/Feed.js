5181bb3b48fb91b633334bc7bceb7587b74e9484
Jose Castor <castor@MacBook-Pro-de-Castor.local>
2019-06-23 10:22:03 -0300

    Style page feed

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,8 +1,41 @@
 import React, { Component } from "react";
 
+import "./Feed.css";
+import more from "../assets/more.svg";
+import like from "../assets/like.svg";
+import comment from "../assets/comment.svg";
+import send from "../assets/send.svg";
 class Feed extends Component {
   render() {
-    return <h1>Feed</h1>;
+    return (
+      <section id="post-list">
+        <article>
+          <header>
+            <div className="user-info">
+              <span>The Castor</span>
+              <span className="place">Rio Claro</span>
+            </div>
+            <img src={more} alt="Mais" />
+          </header>
+
+          <img src="" alt="" />
+          <footer>
+            <div className="actions">
+              <img src={like} alt="" />
+              <img src={comment} alt="" />
+              <img src={send} alt="" />
+            </div>
+
+            <strong>1000 curtidas</strong>
+
+            <p>
+              bla bla bla bvla
+              <span>#react #react-native #nodejs</span>
+            </p>
+          </footer>
+        </article>
+      </section>
+    );
   }
 }
 
