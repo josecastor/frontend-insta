2b8e437fa357189ca8b383acd34a82ae33485f70
Jose Castor <castor@MacBook-Pro-de-Castor.local>
2019-06-24 08:49:41 -0300

    Socket.io implemented on feed

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from "../services/api";
+import io from "socket.io-client";
 
 import "./Feed.css";
 
@@ -14,9 +15,29 @@ class Feed extends Component {
   };
 
   async componentDidMount() {
+    this.registerToSocket();
+
     const response = await api.get("posts");
     this.setState({ feed: response.data.posts });
   }
+
+  registerToSocket = () => {
+    const socket = io("http://localhost:3333");
+
+    // atualizando os post com o ultimo post em primero
+    socket.on("post", newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    });
+
+    // atualizando os likes
+    socket.on("like", likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post =>
+          post._id === likedPost._id ? likedPost : post
+        )
+      });
+    });
+  };
 
   handleLike = async id => {
     await api.post(`/posts/${id}/like`);
