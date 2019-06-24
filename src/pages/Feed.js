e1b4cdad0f0e3ee73100e85807ea371776a04a77
Jose Castor <castor@MacBook-Pro-de-Castor.local>
2019-06-24 07:46:38 -0300

    Add integration with backend read all posts

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,39 +1,55 @@
 import React, { Component } from "react";
+import api from "../services/api";
 
 import "./Feed.css";
+
 import more from "../assets/more.svg";
 import like from "../assets/like.svg";
 import comment from "../assets/comment.svg";
 import send from "../assets/send.svg";
+
 class Feed extends Component {
+  state = {
+    feed: []
+  };
+
+  async componentDidMount() {
+    const response = await api.get("posts");
+    this.setState({ feed: response.data.posts });
+  }
+
   render() {
     return (
       <section id="post-list">
-        <article>
-          <header>
-            <div className="user-info">
-              <span>The Castor</span>
-              <span className="place">Rio Claro</span>
-            </div>
-            <img src={more} alt="Mais" />
-          </header>
+        {this.state.feed.map(post => (
+          <article key={post._id}>
+            <header>
+              <div className="user-info">
+                <span>{post.author}</span>
+                <span className="place">{post.place}</span>
+              </div>
 
-          <img src="" alt="" />
-          <footer>
-            <div className="actions">
-              <img src={like} alt="" />
-              <img src={comment} alt="" />
-              <img src={send} alt="" />
-            </div>
+              <img src={more} alt="Mais" />
+            </header>
 
-            <strong>1000 curtidas</strong>
+            <img src={`http://localhost:3333/files/${post.image}`} alt="" />
 
-            <p>
-              bla bla bla bvla
-              <span>#react #react-native #nodejs</span>
-            </p>
-          </footer>
-        </article>
+            <footer>
+              <div className="actions">
+                <img src={like} alt="" />
+                <img src={comment} alt="" />
+                <img src={send} alt="" />
+              </div>
+
+              <strong>{post.likes} curtidas</strong>
+
+              <p>
+                {post.description}
+                <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        ))}
       </section>
     );
   }
